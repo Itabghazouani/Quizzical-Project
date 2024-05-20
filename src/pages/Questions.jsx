f5aa554040377a3a463d63cad75ec06cca45eb10
itab ghazouani <itab.ghazouani@gmail.com>
2024-05-20 23:04:02 +0200

    Update Questions.jsx

--- src/pages/Questions.jsx
+++ src/pages/Questions.jsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+
 export default function Questions(props) {
+
+  const apiUrl = "https://opentdb.com/api.php?amount=5&category=14&difficulty=hard&type=multiple"
+
+  useEffect(() => {
+    async function getQuestions() {
+      const res = await fetch(apiUrl)
+      const data = await res.json()
+      console.log(data)
+    }
+    getQuestions()
+  }, [])
+
   return (
     <h2>Here are the questions</h2>
   )
